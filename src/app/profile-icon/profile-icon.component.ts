f8ea4a83158bf8a8c933667974fcd3b6fbd18325
PJ <pacojuanquiros@gmail.com>
2019-03-10 13:45:28 +0100

    UserService working
    - Now if we want to fetch user data, we must call (with await) the methods from UserService

--- src/app/profile-icon/profile-icon.component.ts
+++ src/app/profile-icon/profile-icon.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { RdfService } from '../services/solid/rdf.service';
-import { AuthService } from '../services/solid/solid.auth.service';
-import { SolidProfile } from '../models/solid/solid-profile.model';
+import { UserService } from '../services/dechat/user.service';
 
 @Component({
   selector: 'app-profile-icon',
@@ -10,60 +8,20 @@ import { SolidProfile } from '../models/solid/solid-profile.model';
 })
 export class ProfileIconComponent implements OnInit {
 
-  profile: SolidProfile;
-
   userName : string;
   profileImage : string;
 
-  loadingProfile: boolean;
 
-  constructor(private rdf: RdfService, private auth: AuthService) { }
+  constructor(private user: UserService) { }
 
   ngOnInit() {
-    this.loadingProfile = true;
     this.loadProfile();
-
-    // Clear cached profile data
-    // TODO: Remove this code and find a better way to get the old data
-    localStorage.removeItem('oldProfileData');
   }
 
-  // Loads the profile from the rdf service and handles the response
   async loadProfile() {
-    try {
-      this.loadingProfile = true;
-      const profile = await this.rdf.getProfile();
-      if (profile) {
-        this.profile = profile;
-        this.auth.saveOldUserData(profile);
-      }
-
-      this.loadingProfile = false;
-      if (this.profile) {
-        this.setupFromProfile();
-      } else {
-        this.setupDefault()
-      }
-
-    } catch (error) {
-      console.log(`Error: ${error}`);
-    }
-
+    this.userName = await this.user.getUserName();
+    this.profileImage = await this.user.getProfileImage();
   }
-
-
-
-  private setupFromProfile() {
-    this.userName = this.profile.fn;
-    this.profileImage = this.profile.image ? this.profile.image : '/assets/images/profile.png';
-  }
-
-  private setupDefault() {
-    this.userName = "Unknown";
-    this.profileImage = '/assets/images/profile.png';
-  }
-
-
 
 
 }
--- src/app/services/dechat/user.service.ts
+++ src/app/services/dechat/user.service.ts
@@ -13,72 +13,56 @@ export class UserService {
   profile: SolidProfile;
   loadingProfile : boolean;
 
-  //userName: string;
   contacts : User[];
+  userName : string;
+  profileImage : string;
 
-  constructor(
-    private rdf : RdfService,
-    private auth: AuthService
-  ) {
-/*
+
+  constructor(private rdf: RdfService, private auth: AuthService) {
+
     this.loadingProfile = true;
-    this.rdf.getSession();
     this.loadProfile();
 
     // Clear cached profile data
     // TODO: Remove this code and find a better way to get the old data
     localStorage.removeItem('oldProfileData');
-*/
-    //this.rdf.getProfile().then(response => this.userName = response.fn);
-    //this.loadContacts();
   }
 
-
   // Loads the profile from the rdf service and handles the response
-  private async loadProfile() {
+  async loadProfile() {
+    if (!this.loadingProfile)
+      return;
 
-    await this.rdf.getSession();
-    console.log("Loading user profile...");
     try {
       this.loadingProfile = true;
       const profile = await this.rdf.getProfile();
       if (profile) {
         this.profile = profile;
         this.auth.saveOldUserData(profile);
-        this.loadContacts();
-        console.log("USER SERVICE READY!");
-        console.log(this.profile.fn);
       }
 
       this.loadingProfile = false;
-      this.setupProfileData();
-      
+      if (this.profile) {
+        this.setupFromProfile();
+      } else {
+        this.setupDefault()
+      }
+
     } catch (error) {
-      console.log(`Error loading profile: ${error}`);
+      console.log(`Error: ${error}`);
     }
 
   }
 
-
-  private setupProfileData() {
-    if (this.profile) {
-      //this.profileImage = this.profile.image ? this.profile.image : '/assets/images/profile.png';
-    } else {
-      //this.profileImage = '/assets/images/profile.png';
-    }
+  private setupFromProfile() {
+    this.userName = this.profile.fn;
+    this.profileImage = this.profile.image ? this.profile.image : '/assets/images/profile.png';
+    this.loadContacts();
   }
 
-  getUserName() : string {
-    if (this.profile) {
-      return this.profile.fn;
-    } else {
-      return "";
-    }
-  }
-
-
-  getContacts() : Observable<User[]> {
-    return of(this.contacts);
+  private setupDefault() {
+    this.userName = "Unknown";
+    this.profileImage = '/assets/images/profile.png';
   }
 
   private async loadContacts() {
@@ -91,6 +75,29 @@ export class UserService {
       this.contacts.push(contact);
     });
   }
+
+
+
+
+
+
+
+  async getUserName() : Promise<string> {
+    await this.loadProfile();
+    return this.userName;
+  }
+
+  async getProfileImage() : Promise<string> {
+    await this.loadProfile();
+    return this.profileImage;
+  }
+
+
+  getContacts() : Observable<User[]> {
+    return of(this.contacts);
+  }
+
+  
 
 
 }
