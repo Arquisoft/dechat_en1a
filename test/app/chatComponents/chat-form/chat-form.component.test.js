c2431990aa2d281d20b0506bc6d6f13c62f7f11c
Hugo Fonseca Díaz <UO258318@uniovi.es>
2019-03-11 23:05:11 +0100

    Tests - Testing user model.
    A couple of tests for the constructor of the user model.

--- test/app/chatComponents/chat-form/chat-form.component.test.js
+++ test/app/chatComponents/chat-form/chat-form.component.test.js
@@ -1,6 +1,6 @@
 // We need the ChatFormComponent and the ChatService modules
-var cFormComp = require('src/app/chatComponents/chat-form/chat-form.component');
-var cServ = require('src/app/services/dechat/chat.service');
+const cFormComp = require('../../../../src/app/chatComponents/chat-form/chat-form.component').ChatFormComponent;
+const cServ = require('../../../../src/app/services/dechat/chat.service').ChatService;
 
 // We need to load the assert module
 var assert = require('assert');
@@ -8,7 +8,7 @@ var assert = require('assert');
 // Now we define the tests
 describe('Chat-Form', function() { 
     it('Should have an empty message when created', function() {
-        var chatForm = new cFormComp.ChatFormComponent(new cServ.ChatService());
+        var chatForm = new cFormComp(new cServ());
         assert.equal(chatForm.message, "");
     });  
 });
