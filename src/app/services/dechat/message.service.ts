43642706d377e714bd23b700580ad987d37e1398
Iván Prieto Labrador <uo258306@uniovi.es>
2019-04-02 09:56:24 +0200

    Refactoring of services

--- src/app/services/dechat/message.service.ts
+++ src/app/services/dechat/message.service.ts
@@ -1,224 +1,201 @@
-import { Injectable } from '@angular/core';
-import { ChatMessage } from 'src/app/models/dechat/chat-message.model';
-import { ChatInfo } from 'src/app/models/dechat/chat-info.model';
-import { MessageBundle } from 'src/app/models/dechat/message-bundle.model';
-import { FilesService } from './files.service';
-import { User } from 'src/app/models/dechat/user.model';
-import { Observable, of } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {Observable, of} from 'rxjs';
+import {ChatInfo} from 'src/app/models/dechat/chat-info.model';
+import {ChatMessage} from 'src/app/models/dechat/chat-message.model';
+import {MessageBundle} from 'src/app/models/dechat/message-bundle.model';
+import {User} from 'src/app/models/dechat/user.model';
+import {FilesService} from './files.service';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class MessageService {
 
-  user : User;
-  chatMessage : ChatMessage;
+    user: User;
+    chatMessage: ChatMessage;
 
-  currentChat: ChatInfo;
-  currentMessages : ChatMessage[];
-  currentBundle : MessageBundle; 
-  currentChatUrl : string;
+    currentChat: ChatInfo;
+    currentMessages: ChatMessage[];
+    currentBundle: MessageBundle;
+    currentChatUrl: string;
 
-  // We will store the chats locally in a hash map
-  // This way, we'll be able to directly read messages
-  // That have already been fetched.
-  chatMap : Map<ChatInfo, MessageBundle[]>;
+    // We will store the chats locally in a hash map
+    // This way, we'll be able to directly read messages
+    // That have already been fetched.
+    chatMap: Map<ChatInfo, MessageBundle[]>;
 
-
-  constructor(private files : FilesService) 
-  { 
-    this.currentMessages = [];
-    this.chatMap = new Map<ChatInfo, MessageBundle[]>();
-  }
-
-
-  setCurrentUser(user: User) {
-    this.user = user;
-  }
-
-  async setCurrentChat(chat: ChatInfo) {
-
-    if (this.currentChat === chat)
-      return;
-    this.currentChat = chat;
-
-    if (!this.chatMap.has(chat)) {
-      this.chatMap.set(chat, []);
-    }
-    
-    // Clear current messaged
-    while(this.currentMessages.length > 0)
-      this.currentMessages.pop();
-
-    this.currentChatUrl = await this.files.getChatUrl(this.user, chat);
-
-    // Retrieve the existing bundles in the map
-    var bundles = this.chatMap.get(this.currentChat);
-    bundles.forEach(b => this.processFetchedMessages(b.messages));
-
-    await this.fetchMessageBundles();
-    
-  }
-
-
-  getFullTimeStamp() {
-    const now = new Date();
-
-    const date = now.getUTCFullYear() + '_' +
-                 (now.getUTCMonth() + 1) + '_' +
-                 now.getUTCDate();
-
-    const time = now.getUTCHours() + '_' +
-                 now.getUTCMinutes() + '_' +
-                 now.getSeconds() + '_' +
-                 now.getMilliseconds();
-    
-    return date + ' ' + time;
-  }
-
-
-
-
-
-
-
-
-
-  /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-  /*                                                           */
-  /*                     MESSAGE MANAGEMENT                    */
-  /*                                                           */
-  /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-
-
-  getMessages(): Observable<ChatMessage[]> {
-    return of(this.currentMessages);
-  }
-
-
-  sendMessage(msg: string) {
-
-    var message = this.createMessage(msg);
-
-    // Add the message to the bundle.
-    // Create a new bundle if the current one is full
-    if (!this.currentBundle.addMessage(message)) {
-      this.currentBundle = this.createBundle(this.getFullTimeStamp());
-      this.currentBundle.addMessage(message);
+    constructor(private files: FilesService) {
+        this.currentMessages = [];
+        this.chatMap = new Map<ChatInfo, MessageBundle[]>();
     }
 
-    this.currentMessages.push(message);
-    this.pushMessage(message);
+    setCurrentUser(user: User) {
+        this.user = user;
+    }
 
-    console.log("[Message sent] : " + msg);
-  }
+    async setCurrentChat(chat: ChatInfo) {
 
+        if (this.currentChat === chat) {
+            return;
+        }
+        this.currentChat = chat;
 
-  private createMessage(msg: string) : ChatMessage {
-    var message = new ChatMessage();
-    message.message = msg;
-    message.userName = this.user.userName;
-    return message;
-  }
+        if (!this.chatMap.has(chat)) {
+            this.chatMap.set(chat, []);
+        }
 
-  private pushMessage(msg : ChatMessage) {
-    var str = JSON.stringify(msg);
-    
-    // Push it inside a bundle in all pods of the chat
-    this.currentChat.users.forEach(async user => {
-      var chatPath = await this.files.getChatUrl(this.user, this.currentChat);
-      var path = chatPath + this.currentBundle.bundleId
-          + "/" + this.getFullTimeStamp() + ".txt";
+        // Clear current messaged
+        while (this.currentMessages.length > 0) {
+            this.currentMessages.pop();
+        }
 
-      this.files.createFile(path, str);
-    });
-  }
+        this.currentChatUrl = await this.files.getChatUrl(this.user, chat);
 
-  
-  private processFetchedMessages(messages : ChatMessage[]) {
+        // Retrieve the existing bundles in the map
+        const bundles = this.chatMap.get(this.currentChat);
+        bundles.forEach((b) => this.processFetchedMessages(b.messages));
 
-    // TODO sorth them
-    messages.sort((a, b) => a.date.getTime() - b.date.getTime());
-    
-    messages.forEach(m => this.currentMessages.push(m));
-  }
+        await this.fetchMessageBundles();
 
+    }
 
+    getFullTimeStamp() {
+        const now = new Date();
 
-  private async getMessageFromFile(url:string) : Promise<ChatMessage> {
-    var msg;
-    var json;
-    await this.files.readFile(url).then(body => json = JSON.parse(body));
+        const date = now.getUTCFullYear() + '_' +
+            (now.getUTCMonth() + 1) + '_' +
+            now.getUTCDate();
 
-    // TODO get all the message data, I guess
-    msg = new ChatMessage(json.message);
-    return msg;
-  }
+        const time = now.getUTCHours() + '_' +
+            now.getUTCMinutes() + '_' +
+            now.getSeconds() + '_' +
+            now.getMilliseconds();
 
+        return date + ' ' + time;
+    }
 
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+    /*                                                           */
+    /*                     MESSAGE MANAGEMENT                    */
+    /*                                                           */
 
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
+    getMessages(): Observable<ChatMessage[]> {
+        return of(this.currentMessages);
+    }
 
+    sendMessage(msg: string) {
 
+        const message = this.createMessage(msg);
 
+        // Add the message to the bundle.
+        // Create a new bundle if the current one is full
+        if (!this.currentBundle.addMessage(message)) {
+            this.currentBundle = this.createBundle(this.getFullTimeStamp());
+            this.currentBundle.addMessage(message);
+        }
 
+        this.currentMessages.push(message);
+        this.pushMessage(message);
 
-  /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-  /*                                                           */
-  /*                     BUNDLE MANAGEMENT                     */
-  /*                                                           */
-  /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+        console.log('[Message sent] : ' + msg);
+    }
 
+    private createMessage(msg: string): ChatMessage {
+        const message = new ChatMessage();
+        message.message = msg;
+        message.userName = this.user.userName;
+        return message;
+    }
 
-  // TODO fetch messages in bundles
-  async fetchMessageBundles() {
-      
-    // TODO for each folder in the chat folder, create  bundle
-    await this.files.readFolder(this.currentChatUrl).then(
-        // TODO check if they already exist
-        async files => await files.forEach(async f => { 
-          await this.fetchMessageBundle(f);
-        })
-    );
+    private pushMessage(msg: ChatMessage) {
+        const str = JSON.stringify(msg);
 
-  }
+        // Push it inside a bundle in all pods of the chat
+        this.currentChat.users.forEach(async (user) => {
+            const chatPath = await this.files.getChatUrl(this.user, this.currentChat);
+            const path = chatPath + this.currentBundle.bundleId
+                + '/' + this.getFullTimeStamp() + '.txt';
 
-  // Feteches all the messages in a given bundle.
-  private async fetchMessageBundle(bundleUrl: string) {
+            this.files.createFile(path, str);
+        });
+    }
 
-    var id = bundleUrl.replace(this.currentChatUrl, "").replace("/", "");
-    var bundle = this.getBundle(id);
+    private processFetchedMessages(messages: ChatMessage[]) {
 
-    // Read all messages inside the bundle folder
-    await this.files.readFolder(bundleUrl).then(
-      async files => await files.forEach(async f => { 
-        await this.getMessageFromFile(f)      })
-    );
+        // TODO sorth them
+        messages.sort((a, b) => a.date.getTime() - b.date.getTime());
 
-    this.processFetchedMessages(bundle.messages);
-    this.chatMap.get(this.currentChat).push(bundle);
-  }
+        messages.forEach((m) => this.currentMessages.push(m));
+    }
 
-  
-  private getBundle(bundleId: string) : MessageBundle {
-    this.chatMap.get(this.currentChat).forEach(bundle => {
-      if (bundle.bundleId === bundleId)
+    private async getMessageFromFile(url: string): Promise<ChatMessage> {
+        let msg;
+        let json;
+        await this.files.readFile(url).then((body) => json = JSON.parse(body));
+
+        // TODO get all the message data, I guess
+        msg = new ChatMessage(json.message);
+        return msg;
+    }
+
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+    /*                                                           */
+    /*                     BUNDLE MANAGEMENT                     */
+    /*                                                           */
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+    // TODO fetch messages in bundles
+    async fetchMessageBundles() {
+
+        // TODO for each folder in the chat folder, create  bundle
+        await this.files.readFolder(this.currentChatUrl).then(
+            // TODO check if they already exist
+            async (files) => await files.forEach(async (f) => {
+                await this.fetchMessageBundle(f);
+            }),
+        );
+
+    }
+
+    // Fetches all the messages in a given bundle.
+    private async fetchMessageBundle(bundleUrl: string) {
+
+        const id = bundleUrl.replace(this.currentChatUrl, '').replace('/', '');
+        const bundle = this.getBundle(id);
+
+        // Read all messages inside the bundle folder
+        await this.files.readFolder(bundleUrl).then(
+            async (files) => await files.forEach(async (f) => {
+                await this.getMessageFromFile(f);
+            }),
+        );
+
+        this.processFetchedMessages(bundle.messages);
+        this.chatMap.get(this.currentChat).push(bundle);
+    }
+
+    private getBundle(bundleId: string): MessageBundle {
+        this.chatMap.get(this.currentChat).forEach((bundle) => {
+            if (bundle.bundleId === bundleId) {
+                return bundle;
+            }
+        });
+
+        return this.createBundle(bundleId);
+    }
+
+    private createBundle(id: string): MessageBundle {
+        const bundle = new MessageBundle(this.currentChat, id);
+
+        // Check the existence in all pods in the chat
+        this.currentChat.users.forEach(async (user) => {
+            const path = await this.files.getChatUrl(this.user, this.currentChat);
+            this.files.checkFolderExistence(path + bundle.bundleId + '/');
+        });
+
         return bundle;
-    });
-
-    return this.createBundle(bundleId);
-  }
-
-  private createBundle(id : string) : MessageBundle {
-    var bundle = new MessageBundle(this.currentChat, id);
-    
-    // Check the existence in all pods in the chat
-    this.currentChat.users.forEach(async user => {
-      var path = await this.files.getChatUrl(this.user, this.currentChat);
-      this.files.checkFolderExistence(path + bundle.bundleId + "/");
-    });
-
-    return bundle;
-  }
+    }
 
 }
--- src/app/services/dechat/user.service.ts
+++ src/app/services/dechat/user.service.ts
@@ -1,136 +1,128 @@
-import { Injectable } from '@angular/core';
-import { RdfService } from '../solid/rdf.service';
-import { Observable, of } from 'rxjs';
-import { User } from 'src/app/models/dechat/user.model';
-import { SolidProfile } from 'src/app/models/solid/solid-profile.model';
-import { AuthService } from '../solid/solid.auth.service';
+import {Injectable} from '@angular/core';
+import {Observable, of} from 'rxjs';
+import {User} from 'src/app/models/dechat/user.model';
+import {SolidProfile} from 'src/app/models/solid/solid-profile.model';
+import {RdfService} from '../solid/rdf.service';
+import {AuthService} from '../solid/solid.auth.service';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class UserService {
 
-  private profile: SolidProfile;
-  private loadingProfile : boolean;
-  private profileLoaded : boolean;
+    private profile: SolidProfile;
+    private loadingProfile: boolean;
+    private profileLoaded: boolean;
 
-  private user : User;
-  private contacts : User[];
+    private user: User;
+    private contacts: User[];
 
+    constructor(private rdf: RdfService, private auth: AuthService) {
 
-  constructor(private rdf: RdfService, private auth: AuthService) {
+        console.log('User service constructor');
+        this.user = null;
+        this.loadingProfile = true;
+        this.profileLoaded = false;
+        this.contacts = [];
+        this.loadProfile();
 
-    console.log("User service constructor");
-    this.user = null;
-    this.loadingProfile = true;
-    this.profileLoaded = false;
-    this.contacts = [];
-    this.loadProfile();
-
-    // Clear cached profile data
-    // TODO: Remove this code and find a better way to get the old data
-    localStorage.removeItem('oldProfileData');
-  }
-
-  // Loads the profile from the rdf service and handles the response
-  async loadProfile() {
-
-    if (this.profileLoaded)
-      return;
-
-    if (!this.loadingProfile)
-      return;
-
-    try {
-      this.loadingProfile = true;
-      const profile = await this.rdf.getProfile();
-      if (profile) {
-        this.profile = profile;
-        this.auth.saveOldUserData(profile);
-      }
-
-      this.loadingProfile = false;
-      this.profileLoaded = true;
-      if (this.profile) {
-        this.setupFromProfile();
-      } else {
-        this.setupDefault()
-      }
-
-    } catch (error) {
-      console.log(`Error: ${error}`);
+        // Clear cached profile data
+        // TODO: Remove this code and find a better way to get the old data
+        localStorage.removeItem('oldProfileData');
     }
-  }
 
-  private setupFromProfile() {
+    // Loads the profile from the rdf service and handles the response
+    async loadProfile() {
 
-    this.user = new User(this.rdf.getWebID());
-    this.user.url = this.rdf.getWebID();
-    this.user.userName = this.profile.fn;
-    this.user.status = 'online';
-    this.user.profileImage = this.profile.image ? this.profile.image : '/assets/images/profile.png';
-    this.loadContacts();
-  }
+        if (this.profileLoaded) {
+            return;
+        }
 
-  private setupDefault() {
-    console.log("UserService failed fetching user data. Loading default user.")
-    this.user = new User("");
-  }
+        if (!this.loadingProfile) {
+            return;
+        }
 
-  private async loadContacts() {
-    console.log("Fetching contacts...");
-    
-    var contacts = await this.rdf.getContacts();
-    console.log("Contact count = " + contacts.length);
+        try {
+            this.loadingProfile = true;
+            const profile = await this.rdf.getProfile();
+            if (profile) {
+                this.profile = profile;
+                this.auth.saveOldUserData(profile);
+            }
 
-    while(this.contacts.length > 0)
-      this.contacts.pop();
+            this.loadingProfile = false;
+            this.profileLoaded = true;
+            if (this.profile) {
+                this.setupFromProfile();
+            } else {
+                this.setupDefault();
+            }
 
-      // TODO
-    /*await contacts.forEach(async element => {
+        } catch (error) {
+            console.log(`Error: ${error}`);
+        }
+    }
 
-      console.log("Contact: " + element.value);
-      await this.rdf.fetcher.load(element.value);
-      var name = this.rdf.getValueFromVcard('fn', element.value);
-      console.log("Contact name = " + name);
-      var c = new User(name);
-      this.contacts.push(c);
-    });*/
+    private setupFromProfile() {
 
-    contacts.forEach(element => {
-      var c = new User(element.value);
-      this.contacts.push(c);
-    });
-  }
+        this.user = new User(this.rdf.getWebID());
+        this.user.url = this.rdf.getWebID();
+        this.user.userName = this.profile.fn;
+        this.user.status = 'online';
+        this.user.profileImage = this.profile.image ? this.profile.image : '/assets/images/profile.png';
+        this.loadContacts();
+    }
 
+    private setupDefault() {
+        console.log('UserService failed fetching user data. Loading default user.');
+        this.user = new User('');
+    }
 
+    private async loadContacts() {
+        console.log('Fetching contacts...');
 
+        const contacts = await this.rdf.getContacts();
+        console.log('Contact count = ' + contacts.length);
 
+        while (this.contacts.length > 0) {
+            this.contacts.pop();
+        }
 
+        // TODO
+        /*await contacts.forEach(async element => {
 
-  async getUser() : Promise<User> {
-    await this.loadProfile();
-    return this.user;
-  }
+          console.log("Contact: " + element.value);
+          await this.rdf.fetcher.load(element.value);
+          var name = this.rdf.getValueFromVcard('fn', element.value);
+          console.log("Contact name = " + name);
+          var c = new User(name);
+          this.contacts.push(c);
+        });*/
 
+        contacts.forEach((element) => {
+            const c = new User(element.value);
+            this.contacts.push(c);
+        });
+    }
 
-  async getUserName() : Promise<string> {
-    await this.loadProfile();
-    return this.user.userName;
-  }
+    async getUser(): Promise<User> {
+        await this.loadProfile();
+        return this.user;
+    }
 
-  async getProfileImage() : Promise<string> {
-    await this.loadProfile();
-    return this.user.profileImage;
-  }
+    async getUserName(): Promise<string> {
+        await this.loadProfile();
+        return this.user.userName;
+    }
 
+    async getProfileImage(): Promise<string> {
+        await this.loadProfile();
+        return this.user.profileImage;
+    }
 
-  async getContacts() : Promise<User[]> {
-    await this.loadProfile();
-    return this.contacts;
-  }
-
-  
-
+    async getContacts(): Promise<User[]> {
+        await this.loadProfile();
+        return this.contacts;
+    }
 
 }
