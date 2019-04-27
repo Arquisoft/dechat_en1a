38a9e9221d6236c96d2bfeb292fb546412484416
uo251119 <uo251119@uniovi.es>
2019-04-27 19:12:18 +0200

    more fixes in new-chat-dialog

--- src/app/dialogComponents/new-multimedia-dialog/new-multimedia-dialog.component.spec.ts
+++ src/app/dialogComponents/new-multimedia-dialog/new-multimedia-dialog.component.spec.ts
@@ -1,25 +1,36 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { NewMultimediaDialogComponent } from './new-multimedia-dialog.component';
+import {NewMultimediaDialogComponent} from './new-multimedia-dialog.component';
+
+import {FormsModule} from '@angular/forms';
+
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
 
 describe('NewMultimediaDialogComponent', () => {
-  let component: NewMultimediaDialogComponent;
-  let fixture: ComponentFixture<NewMultimediaDialogComponent>;
+    let component: NewMultimediaDialogComponent;
+    let fixture: ComponentFixture<NewMultimediaDialogComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ NewMultimediaDialogComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                FormsModule,
+            ],
+            declarations: [NewMultimediaDialogComponent],
+            providers: [
+                { provide: MAT_DIALOG_DATA, useValue: {} },
+                { provide: MatDialogRef, useValue: {} },
+            ],
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(NewMultimediaDialogComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(NewMultimediaDialogComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
