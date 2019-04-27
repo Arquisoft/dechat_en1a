612092d49829fac1f89b5c29feb0182db245710c
Hugo Fonseca Díaz <UO258318@uniovi.es>
2019-04-27 19:47:54 +0200

    Documenting the code - Inbox and message services.

--- src/app/services/dechat/inbox.service.ts
+++ src/app/services/dechat/inbox.service.ts
@@ -12,12 +12,29 @@ import { ChatMessage } from 'src/app/models/dechat/chat-message.model';
 })
 export class InboxService {
 
+  /**
+   * The user of the application.
+   * 
+   * @type {User}
+   */
   private user : User;
-  private newElements : InboxElement[];
 
+  /**
+   * The new elements of the inbox.
+   * 
+   * @type {InboxElement}
+   */
+  private newElements : InboxElement[];
   private onElementFoundCallbacks;
 
-
+  /**
+   * Creates an inbox service.
+   * 
+   * @param files 
+   *          The files service.
+   * @param users 
+   *          The users service.
+   */
   constructor(
     private files : FilesService,
     private users : UserService
@@ -27,7 +44,9 @@ export class InboxService {
     this.setUp();
   }
 
-
+  /**
+   * Sets up the inbox. Called when the service is created.
+   */
   private async setUp() {
     await 4;
 
@@ -36,14 +55,14 @@ export class InboxService {
     setInterval(this.checkInbox.bind(this), 500/*2000*/);
   }
 
-
   addOnElementFoundCallback(callback) {
     this.onElementFoundCallbacks.push(callback);
   }
 
-
-  // This function is called periodically.
-  // It checks if there exist new files in the inbox.
+  /**
+   * This function is called periodically, it checks if 
+   * there are any new files in the inbox.
+   */
   private async checkInbox() {
 
     if (this.user == undefined) {
@@ -66,9 +85,9 @@ export class InboxService {
     this.processNewElements();
   }
 
-
-
-
+  /**
+   * Proccesses any new element in the inbox.
+   */
   private processNewElements() {
     this.newElements.forEach( element => {
       this.onElementFoundCallbacks.forEach(callback => { callback(element); });
@@ -76,9 +95,12 @@ export class InboxService {
     this.newElements = [];
   }
 
-
-
-  // Takes an array of urls and processes the requests
+  /**
+   * Takes an array of urls and processes the requests.
+   * 
+   * @param files 
+   *          The files' URLs.
+   */
   private async addInboxFiles(files : string[]) {
 
     for (var i = 0; i < files.length; i ++) {
@@ -94,19 +116,20 @@ export class InboxService {
   }
 
 
-
-
-
-
-
   /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
   /*                                                           */
   /*                         REQUESTS                          */
   /*                                                           */
   /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-
-
+  /**
+   * Sends a chat request to a given user.
+   * 
+   * @param toUser 
+   *          The given user.
+   * @param chat 
+   *          The chat that is requested.
+   */
   public sendChatRequest(toUser: User, chat: ChatInfo) {
 
     var request : InboxElement;
@@ -121,6 +144,16 @@ export class InboxService {
   }
 
 
+  /**
+   * Sends a new message to the chat to a given user.
+   * 
+   * @param toUser 
+   *          The given user,
+   * @param chat 
+   *          The chat to which the messages is sent.
+   * @param message 
+   *          The message.
+   */
   public sendNewMessage(toUser: User, chat : ChatInfo, message : ChatMessage) {
 
     var request : InboxElement;
@@ -135,7 +168,14 @@ export class InboxService {
     this.sendRequest(request, filename);
   }
 
-
+  /**
+   * Sends a request given the inbox element and a filename.
+   * 
+   * @param inboxElement 
+   *          The inbox element.
+   * @param filename 
+   *          The filename of the file to be created.
+   */
   private sendRequest(inboxElement : InboxElement, filename: string) {
     
     console.log("Sending request...")
