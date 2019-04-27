c371a4f6c3412837eecb5f4cf2498e453cf657bc
uo251119 <uo251119@uniovi.es>
2019-04-27 19:08:17 +0200

    new-chat-dialog component test fixed

--- src/app/utilComponents/user-display-slot/user-display-slot.component.spec.ts
+++ src/app/utilComponents/user-display-slot/user-display-slot.component.spec.ts
@@ -1,27 +1,27 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { UserDisplaySlotComponent } from './user-display-slot.component';
-import { ToastrModule } from 'ngx-toastr';
+import {ToastrModule} from 'ngx-toastr';
+import {UserDisplaySlotComponent} from './user-display-slot.component';
 
 describe('UserDisplaySlotComponent', () => {
-  let component: UserDisplaySlotComponent;
-  let fixture: ComponentFixture<UserDisplaySlotComponent>;
+    let component: UserDisplaySlotComponent;
+    let fixture: ComponentFixture<UserDisplaySlotComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ UserDisplaySlotComponent ],
-      imports: [ ToastrModule.forRoot() ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [UserDisplaySlotComponent],
+            imports: [ToastrModule.forRoot()],
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(UserDisplaySlotComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(UserDisplaySlotComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
